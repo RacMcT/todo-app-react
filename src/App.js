e0933442c47003fc3811b814e08ac604f1b4bce6
RacMcT <raccmct@gmail.com>
2021-07-12 20:46:28 -0500

    working through functionality in class

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import React  from 'react';
 import './App.css';
 
-function App() {
+
+class App extends React.Component{
+  constructor(props){
+    super(props)
+
+    this.state={
+      isClicked: false, 
+      todos:[],
+      text: ''
+    }
+    // this.handleChange = this.handleChange.bind(this);
+
+  }
+
+  componentDidUpdate() {
+    console.log(this.text);
+  }
+
+ handleChange = (event) => {
+//   console.log(event.target.value);
+  this.setState({
+    text: event.target.value
+  })
+}
+
+
+
+render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" onChange={this.handleChange}/>
     </div>
   );
 }
+}
 
 export default App;
+
+// function App() {
+//   return (
+//     <div className="App">
+//       Hello ACA!
+//     </div>
+//   );
+// }
+
+// export default App;
