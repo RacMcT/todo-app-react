19d489c69ab5c1966d51a3dbba810aba08752c02
RacMcT <raccmct@gmail.com>
2021-07-19 08:38:29 -0500

    created form in react, but still issues with delete and state

--- src/App.js
+++ src/App.js
@@ -1,42 +1,57 @@
-import React  from 'react';
+import React from "react";
 import './App.css';
 
 class App extends React.Component{
   constructor(props){
     super(props)
   }
+//forgot or accidently deleted how to fix this error :(
+  this.state = {
+    isClicked: false,
+    input: "",
+    items: [],
+  };
 
-  render(){
-    return (
-      <div className="App">
-        Hello ACA!!
+inputUpdate = (event) => {
+  this.setState({ input: event.target.value });
+  console.log(event.target.value);
+};
+formSubmit = (event) => {
+  event.preventDefault();
+  this.setState({
+    items: [...this.state.items, this.state.input],
+    input: "",
+  });
+};
+
+render() {
+  return (
+    <div className="App">
+      <h1>This is my ToDo App!</h1>
+      <p>Add an item to the list</p>
+      <form onSubmit={this.formSubmit}>
+          <input
+            type="text"
+            value={this.state.input}
+            onChange={this.inputUpdate}
+          ></input>
+          <Button variant="contained" onClick={this.formSubmit}>
+            Add to List
+          </Button>
+        </form>
+
+        <ul>
+          {this.state.items.map((item, index) => {
+            return <li key={index}>{item}</li>;
+          })}
+        </ul>
+        {console.log(this.state.items)}
       </div>
-    )
+    );
   }
-}
+export default App;
 
-    export default App;
 
-//     this.state={
-//       isClicked: false, 
-//       todos:[],
-//       text: ''
-//     }
-//     // this.handleChange = this.handleChange.bind(this);
-
-//   }
-
-//   componentDidUpdate() {
-//     console.log(this.state.text);
-//   }
-
-//  handleChange = (event) => {
-// //   console.log(event.target.value);
-//   this.setState({
-//     text: event.target.value
-    
-//   })
-// }
 
 // render(){
 //   return (
